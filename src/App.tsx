ac39ec8e09907251b4da1af62f54ca3c223c3a03
nightfury1111111111 <linjay0208@gmail.com>
2022-03-29 09:52:38 +0200

    update bohemia data

--- src/App.tsx
+++ src/App.tsx
@@ -42,10 +42,10 @@ const Context: FC<{ children: ReactNode }> = ({
   children: ReactNode;
 }) => {
   // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
-  const network = WalletAdapterNetwork.Devnet;
+  const network = WalletAdapterNetwork.Mainnet;
 
   // You can also provide a custom RPC endpoint.
-  const endpoint = useMemo(() => clusterApiUrl("devnet"), []);
+  const endpoint = "https://still-patient-sun.solana-mainnet.quiknode.pro"
 
   // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
   // Only the wallets you configure here will be compiled into your application, and only the dependencies
--- src/constants/staking.ts
+++ src/constants/staking.ts
@@ -2,13 +2,13 @@ import { PublicKey } from "@solana/web3.js";
 
 export const stakingGlobals = {
   gemBankProgramId: new PublicKey(
-    "bankHHdqMuaaST4qQk6mkzxGeKPHWmqdgor6Gs8r88m"
+    "5HVwJR5GtJoHfthX8uJvs6VR165ZqPwdUDAQZhytEvaY"
   ),
   gemFarmProgramId: new PublicKey(
-    "farmL4xeBFVXJqtfxCzU9b28QACM7E2W2ctT6epAjvE"
+    "aEEXhBMSEr3y4BmukrZBMCtKApH7aXypW9DvWJzHi2u"
   ),
-  farmId: new PublicKey("BVZD1sejEAoyBx7zYh5r1WmwMz4nARJZJBNgApMvSAzU"),
-  tokenName: "$COIN",
-  tokenDecimals: 0,
-  tokenMint: "9vXVsUgzGt5DQpKjnzseDyA2P3SJpdBuWnjwrSGEo5c9",
+  farmId: new PublicKey("6R6yD9vBViyxGPQu2f1vDpynHCj7sSFLHmefSLhpoHqD"),
+  tokenName: "$WOOP",
+  tokenDecimals: 5,
+  tokenMint: "A3HyGZqe451CBesNqieNPfJ4A9Mu332ui8ni6dobVSLB",
 };
